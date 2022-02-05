95cc165c18ac9260e71a7bf1ded3db900c647983
Rami Ibrahem <ramiabrahem24@gmail.com>
2022-02-05 17:51:58 +0200

    display random photos using javascript

--- app.js
+++ app.js
@@ -1,10 +1,24 @@
 const key = "OymF8oUgJ_u6DIIPyDxq0J31HLH8gruqloJc7nBV_4U";
-const API_URL = `https://api.unsplash.com/photos/random?client_id=${key}&count=10&orientation=squarish`;
+const API_URL = `https://api.unsplash.com/photos/random?client_id=${key}&count=11&orientation=squarish`;
+const photosContainer = document.querySelector(".gallery");
+let photosArray = [];
+
+const displayPhotos = () => {
+  photosArray.forEach((photo) => {
+    const photoContainer = document.createElement("div");
+    photoContainer.classList.add("photo_container");
+    photoContainer.setAttribute("tabindex", "0");
+    const img = document.createElement("img");
+    img.setAttribute("src", photo.urls.regular);
+    photoContainer.append(img);
+    photosContainer.append(photoContainer);
+  });
+};
 
 const getPhotos = async () => {
   const response = await fetch(API_URL);
-  const images = await response.json();
-  console.log(images);
+  photosArray = await response.json();
+  displayPhotos();
 };
 
 getPhotos();
