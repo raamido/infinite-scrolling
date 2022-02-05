9c9fa14aed6519ea5365c16e58c7d68710795feb
Rami Ibrahem <ramiabrahem24@gmail.com>
2022-02-05 11:46:11 +0200

    implement getPhotos function to fetch photos

--- app.js
+++ app.js
@@ -1,2 +1,10 @@
 const key = "OymF8oUgJ_u6DIIPyDxq0J31HLH8gruqloJc7nBV_4U";
 const API_URL = `https://api.unsplash.com/photos/random?client_id=${key}&count=10&orientation=squarish`;
+
+const getPhotos = async () => {
+  const response = await fetch(API_URL);
+  const images = await response.json();
+  console.log(images);
+};
+
+getPhotos();
